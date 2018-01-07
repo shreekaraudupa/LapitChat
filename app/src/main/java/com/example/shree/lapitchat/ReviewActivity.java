d569cf55ebae61860bd52631c5315620073b4ff6
Shreekara  udupa <shreekaraudupa@gmail.com>
2018-01-07 14:10:30 +0530

    Review Activity frontend

--- app/src/main/java/com/example/shree/lapitchat/ReviewActivity.java
+++ app/src/main/java/com/example/shree/lapitchat/ReviewActivity.java
@@ -1,9 +1,12 @@
 package com.example.shree.lapitchat;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
@@ -11,7 +14,9 @@ public class ReviewActivity extends AppCompatActivity {
 
     RadioGroup rgRating;
     RadioButton selectdRating;
-    Button btnReviewSubmit;
+    Button btnReviewSubmit,btnCamera,btnFile;
+    EditText etDescription;
+    private static final int  CAMERA_REQUEST_CODE=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,9 @@ public class ReviewActivity extends AppCompatActivity {
 
         rgRating=findViewById(R.id.rgRating);
         btnReviewSubmit=findViewById(R.id.btnReviewSubmit);
+        btnCamera=findViewById(R.id.btnCamera);
+        btnFile=findViewById(R.id.btnFile);
+        etDescription=findViewById(R.id.etDescription);
 
         btnReviewSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,5 +38,18 @@ public class ReviewActivity extends AppCompatActivity {
 
             }
         });
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent,CAMERA_REQUEST_CODE);
+                //After user caputers image it will call onActivity result method
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
